22de1b0468564ec578aef1a646b96e342cb8155f
ZaynTamam <tamam.zain78@gmail.com>
2026-04-27 17:13:02 +0700

    Menambahkan kelas BelahKetupat dengan atribut dan metode input untuk mengumpulkan data diagonal dan sisi

--- Tugas.cpp
+++ Tugas.cpp
@@ -2,3 +2,17 @@
 #include <cmath>
 
 using namespace std;
+
+class LayangLayang;
+
+class BelahKetupat {
+private:
+    double d1, d2, sisi;
+
+public:
+    void input() {
+        cout << "--- Input Belah Ketupat ---" << endl;
+        cout << "Diagonal 1: "; cin >> d1;
+        cout << "Diagonal 2: "; cin >> d2;
+        cout << "Sisi      : "; cin >> sisi;
+    }
